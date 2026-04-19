f24b2ac1948d5543c6937f7a47e2f304e90113f5
Sævar Þór Pálsson <thor@megapopgames.com>
2026-04-19 09:38:34 +0000

    asdsa

--- Assets/Scripts/Upgrades/LargerMagUpgrade.cs
+++ Assets/Scripts/Upgrades/LargerMagUpgrade.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 
 public class LargerMagUpgrade : Upgrade
 {
-    public override string UpgradeName { get; set; } = "Larger Magazine"
+    public override string UpgradeName { get; set; } = "Larger Magazine";
     public override string UpgradeDescription { get; set; }
 
     public float MagSizeUpgrade = 1; 
 
     public LargerMagUpgrade (float amount)
     {
+        UpgradeDescription = $"{MagSizeUpgrade} more shots in a mag"; 
         MagSizeUpgrade = amount; 
     }
 
     public override void ApplyUpgrade()
     {
-    
+        
     }
 }
