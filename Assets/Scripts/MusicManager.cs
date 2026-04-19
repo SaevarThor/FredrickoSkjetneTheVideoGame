43a5efeaefcf85cba6517f1fb6c4ee8ada40d53a
Sævar Þór Pálsson <thor@megapopgames.com>
2026-04-19 12:19:47 +0000

    °erw

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,16 +1,44 @@
+using System;
 using UnityEngine;
 
 public class MusicManager : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+{ 
+    [SerializeField] private AudioSource source;
+    [SerializeField] private AudioClip[] clips; 
+    private int clipIndex;
+
+    private float timer; 
+    private float clipLifetime; 
+
+
+    public void SetNewClip()
     {
-        
+        var clip = clips[clipIndex]; 
+        clipLifetime = clip.length; 
+        timer = 0; 
+
+        clipIndex++; 
+        if (clipIndex > clips.Length)
+        {
+            clipIndex = 0;
+        }
+
+        source.clip = clip; 
+        source.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        SetNewClip();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime; 
+
+        if (timer >= clipLifetime)
+        {
+            SetNewClip();
+        }
     }
 }
